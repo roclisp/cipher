d6208c60e967880c240c46a2bb76c69a59897f96
Justin Kramer <jkkramer@gmail.com>
2011-05-05 23:24:10 -0400

    minor test revisions

--- test/cipher/test/core.clj
+++ test/cipher/test/core.clj
@@ -3,7 +3,9 @@
   (:use [clojure.test]))
 
 (def msg1 "Code in Ruby, live longer!")
+(def msg2 "abcdef")
 
 (deftest test-step-1
   (is (= ["CODEI" "NRUBY" "LIVEL" "ONGER"] (step-1 msg1)))
-  (is (= ["ABCDE" "FXXXX"] (step-1 "ABCDEF"))))
+  (is (= ["ABCDE" "FXXXX"] (step-1 msg2)))
+  (is (empty? (step-1 ""))))
